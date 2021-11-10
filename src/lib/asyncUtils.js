c885ed3ab84a2fe916df1450fab27adf32b20d88
(yunjin) <sion11290805@gmail.com>
2021-11-10 15:39:52 +0900

    앨범api 리펙터링

--- src/lib/asyncUtils.js
+++ src/lib/asyncUtils.js
@@ -1,7 +1,22 @@
+export const createAlbumsPromiseThunk = (type, promiseFn) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+  
+  return param => async(dispatch) => {
+    dispatch({type, param});
+    try {
+      const payload = await promiseFn(param);
+      dispatch({type: SUCCESS, payload});
+    }
+    catch (e) {
+      dispatch({type: ERROR, error: e})
+    }
+  }
+}
+
 export const reducerUtils = {
   initial: (initialData = null) => ({
     loading: false,
-    data: null,
+    data: initialData,
     error: null
   }),
   loading: (prevData = null) => ({
@@ -19,4 +34,30 @@ export const reducerUtils = {
     data: null,
     error: error
   })
+}
+
+export const handleAlbumsAsyncActions = (type, key) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+
+  return (state, action) => {
+    switch(action.type) {
+      case type:
+        return {
+          ...state,
+          [key]: reducerUtils.loading()
+        }
+      case SUCCESS:
+        return {
+          ...state,
+          [key]: reducerUtils.success(action.payload)
+        }
+      case ERROR:
+        return {
+          ...state,
+          [key]: reducerUtils.error(action.error)
+        }
+      default:
+        return state;
+    }
+  }
 }
--- src/modules/musics.js
+++ src/modules/musics.js
@@ -1,5 +1,5 @@
 import * as postApi from '../api/album';
-import { reducerUtils } from '../lib/asyncUtils';
+import { createAlbumsPromiseThunk, handleAlbumsAsyncActions, reducerUtils } from '../lib/asyncUtils';
 
 const GET_ALBUMS_ARTISTNAME = 'GET_ALBUMS_ARTISTNAME';
 const GET_ALBUMS_ARTISTNAME_SUCCESS = 'GET_ALBUMS_ARTISTNAME_SUCCESS';
@@ -13,38 +13,9 @@ const GET_ALBUM = 'GET_ALBUM';
 const GET_ALBUM_SUCCESS = 'GET_ALBUM_SUCCESS';
 const GET_ALBUM_ERROR = 'GET_ALBUM_ERROR';
 
-export const getAlbumsArtistName = (searchText) => async (dispatch) => {
-  dispatch({type: GET_ALBUMS_ARTISTNAME});
-  try{
-    const albums = await postApi.getAlbumsArtistName(searchText);
-    dispatch({type: GET_ALBUMS_ARTISTNAME_SUCCESS, albums});
-  }
-  catch(e) { 
-    dispatch({type: GET_ALBUMS_ARTISTNAME_ERROR, error: e})
-  }
-}
-
-export const getAlbumsAlbumName = (searchText) => async (dispatch) => {
-  dispatch({type: GET_ALBUMS_ALBUMNAME});
-  try{
-    const musics = await postApi.getAlbumsAlbumName(searchText);
-    dispatch({type: GET_ALBUMS_ALBUMNAME_SUCCESS, musics});
-  }
-  catch(e) {
-    dispatch({type: GET_ALBUMS_ALBUMNAME_ERROR, error: e})
-  }
-}
-
-export const getAlbum = (url) => async (dispatch) => {
-  dispatch({type: GET_ALBUM});
-  try{
-    const album = await postApi.getAlbum(url);
-    dispatch({type: GET_ALBUM_SUCCESS, album});
-  }
-  catch(e) {
-    dispatch({type: GET_ALBUM_ERROR, error: e})
-  }
-}
+export const getAlbumsArtistName = createAlbumsPromiseThunk(GET_ALBUMS_ARTISTNAME, postApi.getAlbumsArtistName);
+export const getAlbumsAlbumName = createAlbumsPromiseThunk(GET_ALBUMS_ALBUMNAME, postApi.getAlbumsAlbumName);
+export const getAlbum = createAlbumsPromiseThunk(GET_ALBUM, postApi.getAlbum);
 
 const initialState = {
   albums: reducerUtils.initial(),
@@ -55,50 +26,20 @@ const initialState = {
 export default function albums(state = initialState, action) {
   switch(action.type) {
     case GET_ALBUMS_ARTISTNAME:
-      return{
-        ...state,
-        albums: reducerUtils.loading()
-      }
     case GET_ALBUMS_ARTISTNAME_SUCCESS:
-      return{
-        ...state,
-        albums: reducerUtils.success(action.albums)
-      }
     case GET_ALBUMS_ARTISTNAME_ERROR:
-      return{
-        ...state,
-        albums: reducerUtils.error(action.error)
-      }
+      const AlbumsArtistNameHandler = handleAlbumsAsyncActions(GET_ALBUMS_ARTISTNAME, 'albums');
+      return AlbumsArtistNameHandler(state, action);
     case GET_ALBUMS_ALBUMNAME:
-      return{
-        ...state,
-        musics: reducerUtils.loading()
-      }
     case GET_ALBUMS_ALBUMNAME_SUCCESS:
-      return{
-        ...state,
-        musics: reducerUtils.success(action.musics)
-      }
     case GET_ALBUMS_ALBUMNAME_ERROR:
-      return{
-        ...state,
-        musics: reducerUtils.error(action.error)
-      }
+      const AlbumsAlbumsNameHandler = handleAlbumsAsyncActions(GET_ALBUMS_ALBUMNAME, 'musics');
+      return AlbumsAlbumsNameHandler(state, action);
     case GET_ALBUM:
-      return{
-        ...state,
-        album: reducerUtils.loading()
-      }
     case GET_ALBUM_SUCCESS:
-      return{
-        ...state,
-        album: reducerUtils.success(action.album)
-      }
     case GET_ALBUM_ERROR:
-      return{
-        ...state,
-        album: reducerUtils.error(action.error)
-      }
+      const AlbumHandler = handleAlbumsAsyncActions(GET_ALBUM, 'album');
+      return AlbumHandler(state, action);
     default:
       return state;
   }
