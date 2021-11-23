66a39f1efc73b53f4b716501f30ad5408227a62c
(yunjin) <sion11290805@gmail.com>
2021-11-23 10:16:25 +0900

    orderpage 타입스크립트 변환

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -4,7 +4,23 @@ import OrderContainer from '../containers/OrderContainer'
 import OrderPayContainer from '../containers/OrderPayContainer'
 import { leaveOrderPage } from '../modules/order';
 
-export default function OrderPage({history}) {
+type OrderPageProps = {
+  history: History;
+}
+
+type History = {
+  action: string;
+  location: {
+    hash: string;
+    key: string;
+    pathname: string;
+    search: string;
+    state: any;
+  }
+}
+
+export default function OrderPage({ history }: OrderPageProps) {
+  console.log(history)
   const dispatch = useDispatch();
   const onLeaveOrderPage = () => dispatch(leaveOrderPage());
 
